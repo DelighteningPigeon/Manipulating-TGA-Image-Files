5cdc80a275ebf83589ab27db3f0e326fc7a797da
DelighteningPigeon <fillpattana144@gmail.com>
2021-12-12 15:07:01 +0700

    COMMIT

---
+++ cmake-build-debug/CMakeFiles/Manipulating_TGA_Files.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Manipulating_TGA_Files.dir/main.c.obj"
+  "Manipulating_TGA_Files.exe"
+  "Manipulating_TGA_Files.exe.manifest"
+  "Manipulating_TGA_Files.pdb"
+  "libManipulating_TGA_Files.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Manipulating_TGA_Files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

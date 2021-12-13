6858c571034e53eb399acd3928c90badda19bbc1
DelighteningPigeon <fillpattana144@gmail.com>
2021-12-13 09:48:20 +0700

    cmake list working

---
+++ cmake-build-debug/CMakeFiles/tga_reader.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tga_reader.dir/main.c.obj"
+  "libtga_reader.dll.a"
+  "tga_reader.exe"
+  "tga_reader.exe.manifest"
+  "tga_reader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tga_reader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

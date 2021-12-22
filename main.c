4d818c3af4da1698d74a2a7fc8314c2decdb078d
DelighteningPigeon <fillpattana144@gmail.com>
2021-12-22 23:58:06 +0700

    program working

--- main.c
+++ main.c
@@ -5,6 +5,13 @@
 
 typedef uint8_t ubyte;
 
+struct pixel_data{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a;
+};
+
 struct Pixel_arr {
     ubyte* data;
     size_t width;
@@ -14,13 +21,45 @@ struct Pixel_arr {
 
 typedef struct Pixel_arr PixelArray;
 
-int main()
-{
+void function(int input, unsigned long size, struct pixel_data *pixel, ubyte buffer[]) {
+
+    FILE *outfile;
+    outfile = fopen("../edited.tga", "w");
+    fwrite(buffer, 1, 18, outfile);
+
+    if (input == 1) {
+        for (int z = 0; z < size; z++) {
+            putc(pixel[z].g, outfile);
+            putc(pixel[z].b, outfile);
+            putc(pixel[z].r, outfile);
+            putc(pixel[z].a, outfile);
+        }
+    } else if (input == 2) {
+        for (int z = 0; z < size; z++) {
+            putc(pixel[z].g, outfile);
+            putc(pixel[z].r, outfile);
+            putc(pixel[z].b, outfile);
+            putc(pixel[z].a, outfile);
+        }
+    } else if (input == 3) {
+        for (int z = 0; z < size; z++) {
+            putc(pixel[z].r, outfile);
+            putc(pixel[z].g, outfile);
+            putc(pixel[z].b, outfile);
+            putc(pixel[z].a, outfile);
+        }
+    }
+}
+
+int main() {
+
+    int i = 0;
     char c; // 1 byte
     unsigned char u; // 1 byte
 
-    FILE* fp = fopen("./Font.tga", "rb");
-    FILE* outfile = NULL;
+    FILE *fp = fopen("../Font.tga", "rb");
+    FILE *outfile = NULL;
+
     if (fp == NULL) {
         fprintf(stderr, "cannot open file\n");
         return -1;
@@ -31,7 +70,7 @@ int main()
     // errno;
 
     fseek(fp, 0, SEEK_SET);
-    ubyte* data = (ubyte*)malloc(fsize);
+    ubyte *data = (ubyte *) malloc(fsize);
 
     const ubyte tga_sig[7] = {0, 0, 2, 0, 0, 0, 0};
     const size_t tga_header_size = 18;
@@ -39,6 +78,7 @@ int main()
     pix.data = NULL;
 
     int res = fread(data, 1, fsize, fp);
+
     if (res < fsize) {
         fprintf(stderr, "invalid tga file\n");
         goto done;
@@ -53,18 +93,36 @@ int main()
     pix.width = data[12] + (data[13] << 8);
     pix.height = data[14] + (data[15] << 8);
     pix.format = data[16];
+
     if (pix.format != 24) {
         fprintf(stderr, "unsupported tga format\n");
         goto done;
     }
 
     size_t image_size = pix.width * pix.height * pix.format / 8;
+
     if ((tga_header_size + image_size) > fsize) {
         fprintf(stderr, "invalid tga file size\n");
         goto done;
     }
 
-    pix.data = (ubyte*)malloc(image_size);
+    struct pixel_data *pixel;
+    pixel = malloc(pix.width * pix.height * sizeof(struct pixel_data));
+    unsigned char component[5];
+
+    int bytes = 4;
+    fseek(fp, 0, SEEK_SET);
+
+    while (i < pix.width * pix.height) {
+        fread(component, 1, bytes, fp);
+        pixel[i].r = component[2];
+        pixel[i].g = component[1];
+        pixel[i].b = component[0];
+        pixel[i].a = component[3];
+        i++;
+    }
+
+    pix.data = (ubyte *) malloc(image_size);
     memcpy(pix.data, data + 18, image_size);
 
     ubyte buffer[18] = {};
@@ -75,17 +133,20 @@ int main()
     buffer[15] = pix.height >> 8;
     buffer[16] = pix.format;
 
-    outfile = fopen("./test.tga", "wb");
-    fwrite(buffer, 1, sizeof(buffer), outfile);
-    fwrite(pix.data, 1, image_size, outfile);
+    unsigned long size = pix.height * pix.width;
+    int choice;
+    printf("Please enter your choice: ");
+    scanf("%d", &choice);
+    function(choice, size, pixel, buffer);
 
     done:
     fclose(outfile);
 
     free(pix.data);
+    free(pixel);
     free(data);
     fclose(fp);
+
     return 0;
+
 }
-
-
